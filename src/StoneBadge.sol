3bc3c56db00105d18b97492ce839493d19a73d80
xg <xg@xgs-MacBook-Air.local>
2024-08-26 23:07:52 +0800

    stone badge finished

--- src/StoneBadge.sol
+++ src/StoneBadge.sol
@@ -1,6 +1,65 @@
 // SPDX-License-Identifier: BUSL-1.1
-pragma solidity ^0.8.26;
+pragma solidity 0.8.19;
 
-contract StoneBadge {
-    
+import {Attestation} from "@eas/contracts/IEAS.sol";
+import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
+import {ScrollBadgeAccessControl} from "@canvas-contracts/src/badge/extensions/ScrollBadgeAccessControl.sol";
+import {ScrollBadgeSingleton} from "@canvas-contracts/src/badge/extensions/ScrollBadgeSingleton.sol";
+import {ScrollBadgeEligibilityCheck} from "@canvas-contracts/src/badge/extensions/ScrollBadgeEligibilityCheck.sol";
+import {ScrollBadgeNoExpiry} from "@canvas-contracts/src/badge/extensions/ScrollBadgeNoExpiry.sol";
+import {ScrollBadgeSelfAttest} from "@canvas-contracts/src/badge/extensions/ScrollBadgeSelfAttest.sol";
+import {ScrollBadgeNonRevocable} from "@canvas-contracts/src/badge/extensions/ScrollBadgeNonRevocable.sol";
+import {ScrollBadge} from "@canvas-contracts/src/badge/ScrollBadge.sol";
+
+contract StoneBadge is
+    Ownable,
+    ScrollBadgeEligibilityCheck,
+    ScrollBadgeSingleton,
+    ScrollBadgeNoExpiry,
+    ScrollBadgeSelfAttest,
+    ScrollBadgeNonRevocable
+{
+    string public badgeURI;
+    mapping(address => bool) public whitelistAddresses;
+
+    constructor() ScrollBadge(0x4560FECd62B14A463bE44D40fE5Cfd595eEc0113) Ownable() {}
+
+    function onIssueBadge(Attestation calldata attestation)
+        internal 
+        override(ScrollBadge, ScrollBadgeSingleton, ScrollBadgeNoExpiry, ScrollBadgeSelfAttest, ScrollBadgeNonRevocable)
+        returns (bool)
+    {
+        if (!isEligible(attestation.recipient)) {
+            revert("Not Eligible");
+        }
+        return super.onIssueBadge(attestation);
+    }
+
+    function onRevokeBadge(Attestation calldata attestation)
+        internal
+        override(ScrollBadge, ScrollBadgeSingleton, ScrollBadgeNoExpiry, ScrollBadgeSelfAttest)
+        returns (bool)
+    {
+        return super.onRevokeBadge(attestation);
+    }
+
+    function isEligible(address recipient) public view override returns (bool) {
+        return whitelistAddresses[recipient];
+    }
+
+    function badgeTokenURI(bytes32 /*uid*/ ) public view override returns (string memory) {
+        return badgeURI;
+    }
+
+    function setBaseTokenURI(string memory _newBadgeURI) public onlyOwner {
+        badgeURI = _newBadgeURI;
+    }
+
+    function addWhitelistAddresses(address[] memory _addresses) public onlyOwner {
+        uint256 length = _addresses.length;
+        for (uint256 i = 0; i < length;) {
+            whitelistAddresses[_addresses[i]] = true;
+            unchecked { ++i; }
+        }
+    }
 }
--- test/Badge.t.sol
+++ test/Badge.t.sol
@@ -1,16 +1,42 @@
 // SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.8.26;
+pragma solidity 0.8.19;
 
 import {Test, console} from "forge-std/Test.sol";
 import {StoneBadge} from "../src/StoneBadge.sol";
+import {Attestation, IEAS, AttestationRequest, AttestationRequestData} from "@eas/contracts/IEAS.sol";
 
 contract CounterTest is Test {
     StoneBadge public badge;
+    IEAS public eas = IEAS(0xC47300428b6AD2c7D03BB76D05A176058b47E6B0);
+    bytes32 schema = 0xd57de4f41c3d3cc855eadef68f98c0d4edd22d57161d96b7c06d2f4336cc3b49;
 
     function setUp() public {
+        uint256 scroll = vm.createFork("https://rpc.scroll.io");
+        vm.selectFork(scroll);
         badge = new StoneBadge();
     }
 
     function test_Badge() public {
+        address[] memory ads = new address[](1);
+        ads[0] = address(1);
+
+        badge.addWhitelistAddresses(ads);
+        vm.startPrank(address(1));
+        
+        bytes memory payload = abi.encode("StakeStone");
+        bytes memory attestationData = abi.encode(badge, payload);
+
+        AttestationRequestData memory _attData = AttestationRequestData({
+            recipient: address(1),
+            expirationTime: 0,
+            revocable: false,
+            refUID: 0,
+            data: attestationData,
+            value: 0
+        });
+
+        AttestationRequest memory _req = AttestationRequest({schema: schema, data: _attData});
+
+        eas.attest(_req);
     }
 }
